9e6b38e4051863296fe7f09cf8dad0935bbee9c6
Nuzhat Minhaz <nxm1137@rit.edu>
2021-04-20 01:18:17 -0400

    Assets

---
+++ assets/css/modal.js
@@ -0,0 +1,212 @@
+            // NUMBER ONE
+            function imgOnClick(){
+                document.getElementById("myModal").style.display = "block";
+                document.getElementById("img01").src = myImg1.src ;
+            }
+            function spanOnClick() { 
+                document.getElementById("myModal").style.display = "none";
+            }
+                 
+            // NUMBER TWO
+            function imgOnClick2(){
+                document.getElementById("myModal2").style.display = "block";
+                document.getElementById("img02").src = myImg2.src ;
+            }
+            function spanOnClick2() { 
+                document.getElementById("myModal2").style.display = "none";
+            }
+
+            // NUMBER THREE
+            function imgOnClick3(){
+                document.getElementById("myModal3").style.display = "block";
+                document.getElementById("img03").src = myImg3.src ;
+            }
+            function spanOnClick3() { 
+                document.getElementById("myModal3").style.display = "none";
+            }
+                 
+            // NUMBER FOUR
+            function imgOnClick4(){
+                document.getElementById("myModal4").style.display = "block";
+                document.getElementById("img04").src = myImg4.src ;
+            }
+            function spanOnClick4() { 
+                document.getElementById("myModal4").style.display = "none";
+            }
+
+
+            // NUMBER FIVE
+            function imgOnClick5(){
+                document.getElementById("myModal5").style.display = "block";
+                document.getElementById("img05").src = myImg5.src ;
+            }
+
+            function spanOnClick5() { 
+                document.getElementById("myModal5").style.display = "none";
+            }
+
+            // NUMBER SIX
+            function imgOnClick6(){
+                document.getElementById("myModal6").style.display = "block";
+                document.getElementById("img06").src = myImg6.src ;
+            }
+
+            function spanOnClick6() { 
+                document.getElementById("myModal6").style.display = "none";
+            }
+                 
+
+            // NUMBER SEVEN
+            function imgOnClick7(){
+                document.getElementById("myModal7").style.display = "block";
+                document.getElementById("img07").src = myImg7.src ;
+            }
+
+            function spanOnClick7() { 
+                document.getElementById("myModal7").style.display = "none";
+            }
+
+            // NUMBER EIGHT
+            function imgOnClick8(){
+                document.getElementById("myModal8").style.display = "block";
+                document.getElementById("img08").src = myImg8.src ;
+            }
+
+            function spanOnClick8() { 
+                document.getElementById("myModal8").style.display = "none";
+            }
+
+            // NUMBER NINE
+            function imgOnClick9(){
+                document.getElementById("myModal9").style.display = "block";
+                document.getElementById("img09").src = myImg9.src ;
+            }
+
+            function spanOnClick9() { 
+                document.getElementById("myModal9").style.display = "none";
+            }
+
+             // NUMBER TEN
+             function imgOnClick10(){
+                document.getElementById("myModal10").style.display = "block";
+                document.getElementById("img10").src = myImg10.src ;
+            }
+
+            function spanOnClick10() { 
+                document.getElementById("myModal10").style.display = "none";
+            }
+
+             // NUMBER ELEVEN
+             function imgOnClick11(){
+                document.getElementById("myModal11").style.display = "block";
+                document.getElementById("img11").src = myImg11.src ;
+            }
+
+            function spanOnClick11() { 
+                document.getElementById("myModal11").style.display = "none";
+            }
+
+
+            // NUMBER THIRTEEN
+            function imgOnClick13(){
+                document.getElementById("myModal13").style.display = "block";
+                document.getElementById("img13").src = myImg13.src ;
+            }
+
+            function spanOnClick13() { 
+                document.getElementById("myModal13").style.display = "none";
+            }
+
+            // NUMBER FOURTEEN
+            function imgOnClick14(){
+                document.getElementById("myModal14").style.display = "block";
+                document.getElementById("img14").src = myImg14.src ;
+            }
+
+            function spanOnClick14() { 
+                document.getElementById("myModal14").style.display = "none";
+            }
+
+            // NUMBER FIFTEEN
+            function imgOnClick15(){
+                document.getElementById("myModal15").style.display = "block";
+                document.getElementById("img15").src = myImg15.src ;
+            }
+
+            function spanOnClick15() { 
+                document.getElementById("myModal15").style.display = "none";
+            }
+
+            // NUMBER SIXTEEN
+            function imgOnClick16(){
+                document.getElementById("myModal16").style.display = "block";
+                document.getElementById("img16").src = myImg16.src ;
+            }
+
+            function spanOnClick16() { 
+                document.getElementById("myModal16").style.display = "none";
+            }
+
+            // NUMBER SEVENTEEN
+            function imgOnClick17(){
+                document.getElementById("myModal17").style.display = "block";
+                document.getElementById("img17").src = myImg17.src ;
+            }
+
+            function spanOnClick17() { 
+                document.getElementById("myModal17").style.display = "none";
+            }
+
+            // NUMBER NINETEEN
+            function imgOnClick19(){
+                document.getElementById("myModal19").style.display = "block";
+                document.getElementById("img19").src = myImg19.src ;
+            }
+
+            function spanOnClick19() { 
+                document.getElementById("myModal19").style.display = "none";
+            }
+
+            // NUMBER TWENTY
+            function imgOnClick20(){
+                document.getElementById("myModal20").style.display = "block";
+                document.getElementById("img20").src = myImg20.src ;
+            }
+
+            function spanOnClick20() { 
+                document.getElementById("myModal20").style.display = "none";
+            }
+
+            // NUMBER TWENTYONE
+            function imgOnClick21(){
+                document.getElementById("myModal21").style.display = "block";
+                document.getElementById("img21").src = myImg21.src ;
+            }
+
+            function spanOnClick21() { 
+                document.getElementById("myModal21").style.display = "none";
+            }
+
+            // NUMBER TWENTY TWO
+            function imgOnClick22(){
+                document.getElementById("myModal22").style.display = "block";
+                document.getElementById("img22").src = myImg22.src ;
+            }
+
+            function spanOnClick22() { 
+                document.getElementById("myModal22").style.display = "none";
+            }
+
+            // NUMBER TWENTY THREE
+            function imgOnClick23(){
+                document.getElementById("myModal23").style.display = "block";
+                document.getElementById("img23").src = myImg23.src ;
+            }
+
+            function spanOnClick23() { 
+                document.getElementById("myModal23").style.display = "none";
+            }
+
+
+
+            
